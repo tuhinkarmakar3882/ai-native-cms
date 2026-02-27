16c67474f971846d0d4a65668140b107bae7bf9e
Tuhin Karmakar <tuhin.karmakar@equiti.com>
2026-02-27 14:06:44 +0530

    Added Migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
 import * as migration_20260211_023313_initial_schema from './20260211_023313_initial_schema'
+import * as migration_20260227_083623_migration from './20260227_083623_migration'
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20260211_023313_initial_schema.down,
     name: '20260211_023313_initial_schema',
   },
-]
+  {
+    up: migration_20260227_083623_migration.up,
+    down: migration_20260227_083623_migration.down,
+    name: '20260227_083623_migration'
+  },
+];
