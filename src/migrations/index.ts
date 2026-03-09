1f6c27faa6a715179be1088a1ebfa568921662c4
Tuhin Karmakar <tuhin.karmakar@equiti.com>
2026-03-09 16:55:42 +0530

    Added Missing Migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
-import * as migration_20260211_023313_initial_schema from './20260211_023313_initial_schema'
-import * as migration_20260227_083623_migration from './20260227_083623_migration'
+import * as migration_20260211_023313_initial_schema from './20260211_023313_initial_schema';
+import * as migration_20260227_083623_migration from './20260227_083623_migration';
+import * as migration_20260309_112520_migration from './20260309_112520_migration';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260227_083623_migration.up,
     down: migration_20260227_083623_migration.down,
-    name: '20260227_083623_migration'
+    name: '20260227_083623_migration',
+  },
+  {
+    up: migration_20260309_112520_migration.up,
+    down: migration_20260309_112520_migration.down,
+    name: '20260309_112520_migration'
   },
 ];
